13cb883078553821d24f314f92b699ab3908aea3
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-01-05 18:23:29 +0100

    Add basic form with infrastructure

--- src/Web/Web/Controllers/RegisterController.cs
+++ src/Web/Web/Controllers/RegisterController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeMooc.Web.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CodeMooc.Web.Controllers {
 
     [Route("")]
-    public class RegisterController : ControllerBase {
+    public class RegisterController : Controller {
 
         protected DatabaseManager Database { get; }
         protected ILogger<RegisterController> Logger { get; }
@@ -22,12 +23,25 @@ namespace CodeMooc.Web.Controllers {
         }
 
         [HttpGet]
-        public IActionResult Get() {
-            return Ok();
+        public IActionResult Index() {
+            return View("Create");
         }
 
-        [HttpPost]
-        public IActionResult Register() {
+        [HttpPost()]
+        [ValidateAntiForgeryToken]
+        public IActionResult Process(RegistrationViewModel model) {
+            Logger.LogInformation(LoggingEvents.Registration, "Received registration request for email {0}", model.Email);
+
+            if(!ModelState.IsValid) {
+                Logger.LogInformation(LoggingEvents.Registration, "Model binding failed");
+                return View("Create", model);
+            }
+
+            return Ok(true);
+        }
+
+        [HttpPost("verifica")]
+        public IActionResult Validate([FromRoute] int id, [FromQuery] string secret) {
             return Ok(false);
         }
 
---
+++ src/Web/Web/Model/RegistrationViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeMooc.Web.Model {
+
+    public class RegistrationViewModel {
+
+        [Required]
+        [Display(Name = "Nome", Prompt = "Mario")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Cognome", Prompt = "Rossi")]
+        public string Surname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Indirizzo e-mail", Prompt = "mario.rossi@example.org")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data di nascita")]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [Display(Name = "Luogo di nascita")]
+        public string Birthplace { get; set; }
+
+        [Phone]
+        [Display(Name = "Numero di telefono")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password di conferma")]
+        public string PasswordConfirm { get; set; }
+
+        public bool IsTeacher { get; set; }
+
+        public bool HasAttendedMooc { get; set; }
+
+        [Required]
+        public bool AcceptsConditions { get; set; }
+
+        [Required]
+        public bool AcceptsGdpr { get; set; }
+
+    }
+
+}
