83eb52e285c8db7e5ca3499bfcca8ea5536060d2
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-07-18 18:42:57 +0200

    Add registration list access for administrators

---
+++ src/Web/Web/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMooc.Web.Controllers {
+
+    [Route("admin")]
+    public class AdminController : Controller {
+
+        protected DatabaseManager Database { get; }
+        protected ILogger<RegisterController> Logger { get; }
+
+        public AdminController(
+            DatabaseManager database,
+            ILogger<RegisterController> logger
+        ) {
+            Database = database;
+            Logger = logger;
+        }
+
+        [HttpGet("registrations")]
+        public IActionResult ListRegistrations() {
+            var users = from r in Database.Context.Registrations
+                        orderby r.Id ascending
+                        select r;
+
+            var sb = new StringBuilder();
+            sb.AppendLine("# ID, Name, Surname, FiscalCode, Email, Category, RegisteredOn, Confirmed");
+            foreach (var u in users) {
+                sb.AppendJoin(',',
+                    u.Id,
+                    u.Name,
+                    u.Surname,
+                    u.FiscalCode.ToUpperInvariant(),
+                    u.Email.ToLowerInvariant(),
+                    u.Category,
+                    u.RegistrationTimestamp.ToString("R"),
+                    u.ConfirmationTimestamp.HasValue ? "1" : "0"
+                );
+                sb.AppendLine();
+            }
+
+            Response.Headers.TryAdd(HeaderNames.ContentDisposition, new StringValues("attachment; filename=registrations.csv"));
+
+            return Content(sb.ToString(), "text/csv");
+        }
+
+    }
+
+}
